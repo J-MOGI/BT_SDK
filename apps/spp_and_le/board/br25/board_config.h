eddd01e0dffa0ad26c69e92ec719135ee628c819
J-MOGI <j-mogi@kakao.com>
2025-09-19 10:52:57 +0900

    Add AC6969A CodeBlocks project

--- apps/spp_and_le/board/br25/board_config.h
+++ apps/spp_and_le/board/br25/board_config.h
@@ -5,7 +5,8 @@
  *  板级配置选择
  */
 
-#define CONFIG_BOARD_AC636N_DEMO
+// #define CONFIG_BOARD_AC6969A_DEMO
+// #define CONFIG_BOARD_AC636N_DEMO
 // #define CONFIG_BOARD_AC636N_FMY        //CONFIG_APP_FINDMY
 // #define CONFIG_BOARD_AC6368B_DONGLE //CONFIG_APP_DONGLE
 // #define CONFIG_BOARD_AC6363F_DEMO
@@ -14,6 +15,20 @@
 // #define CONFIG_BOARD_AC6369F_DEMO
 // #define CONFIG_BOARD_AC6369C_DEMO
 
+/* 如果工程未通过编译命令行选择具体板级，这里默认使用AC636N配置 */
+#if (!defined(CONFIG_BOARD_AC6969A_DEMO) \
+    && !defined(CONFIG_BOARD_AC636N_DEMO) \
+    && !defined(CONFIG_BOARD_AC636N_FMY) \
+    && !defined(CONFIG_BOARD_AC6368B_DONGLE) \
+    && !defined(CONFIG_BOARD_AC6363F_DEMO) \
+    && !defined(CONFIG_BOARD_AC6366C_DEMO) \
+    && !defined(CONFIG_BOARD_AC6368A_DEMO) \
+    && !defined(CONFIG_BOARD_AC6369F_DEMO) \
+    && !defined(CONFIG_BOARD_AC6369C_DEMO))
+#define CONFIG_BOARD_AC636N_DEMO
+#endif
+
+#include "board_ac6969a_demo_cfg.h"
 #include "board_ac636n_demo_cfg.h"
 #include "board_ac6368b_dongle_cfg.h"
 #include "board_ac6363f_demo_cfg.h"
