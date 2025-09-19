a468fbbb69bfd40b31c2213043864d9d647633ca
J-MOGI <j-mogi@kakao.com>
2025-09-19 10:22:24 +0900

    Add AC6969A demo board with classic audio profiles

--- apps/spp_and_le/board/br25/board_config.h
+++ apps/spp_and_le/board/br25/board_config.h
@@ -5,7 +5,8 @@
  *  板级配置选择
  */
 
-#define CONFIG_BOARD_AC636N_DEMO
+#define CONFIG_BOARD_AC6969A_DEMO
+// #define CONFIG_BOARD_AC636N_DEMO
 // #define CONFIG_BOARD_AC636N_FMY        //CONFIG_APP_FINDMY
 // #define CONFIG_BOARD_AC6368B_DONGLE //CONFIG_APP_DONGLE
 // #define CONFIG_BOARD_AC6363F_DEMO
@@ -14,6 +15,7 @@
 // #define CONFIG_BOARD_AC6369F_DEMO
 // #define CONFIG_BOARD_AC6369C_DEMO
 
+#include "board_ac6969a_demo_cfg.h"
 #include "board_ac636n_demo_cfg.h"
 #include "board_ac6368b_dongle_cfg.h"
 #include "board_ac6363f_demo_cfg.h"
